88acce5924bebb69c2e1785fec0b7d408938ad9f
Harshitha1202 <harshithahrngowda@gmail.com>
2023-03-13 10:50:21 +0530

    first commit

---
+++ src/test/java/practice/jenkinTest.java
@@ -0,0 +1,13 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class jenkinTest {
+	@Test
+	public void setup()
+	{
+		System.out.println("practice Jenkins");
+	}
+	
+
+}
---
+++ src/test/java/practice/practiceTest.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class practiceTest {
+	@Test
+	public void set()
+	{
+		System.out.println("Run Jenkins");
+	}
+
+}
